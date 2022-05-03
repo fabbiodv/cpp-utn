410c5c607e38a6956996b191e4701b3400fef5c2
Fabio Mazzarella <fmazzarella@jusbaires.gob.ar>
2022-05-03 19:35:49 -0300

    Hasta clase 4

---
+++ Clase3.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  /*Ejercicio 1
+  cout << "Por favor ingrese una fecha con el formato AAAAMMDD: " << endl;
+  unsigned fecha;
+  cin >> fecha;
+
+  unsigned anio = fecha / 10000;
+  unsigned dia = fecha % 100;
+  unsigned mes = (fecha % 10000) / 100;
+
+  cout << dia << mes << anio << endl;
+
+  return 0;
+  */
+
+  /*Ejercicio booleano
+  bool resultado = 5 < 6;
+  cout << resultado << endl;
+
+  bool comp1 = 3 != 4;
+  cout << comp1 << endl;
+
+  int x = 2;
+  bool comp2 = x > 9;
+  cout << comp2 << endl;
+
+  return 0;
+  */
+
+  // Ejercicio division
+
+  float numeroA;
+  cin >> numeroA;
+}
